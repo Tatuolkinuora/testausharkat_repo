243a8a9ba6ac460edccbbc37736a4fa3d871f3f5
Tatuolkinuora <ag3462@student.jamk.fi>
2025-12-10 21:38:12 +0200

    eka e2e tehtävä

--- end-to-end/cypress/e2e/spec.cy.js
+++ end-to-end/cypress/e2e/spec.cy.js
@@ -1,14 +1,31 @@
+// Tehvävä 1
+/*
 describe('My first Test', () => {
-  it('clicking "type" shows the right headings', () => {
+  it('Tehtävä 1', () => {
     // Menee suomenkiliselle Wikipedia etusivulle
     cy.visit('https://fi.wikipedia.org/wiki/Wikipedia:Etusivu');
-    // Etsii hakukentän, kirjoittaa siihen "Jamk" ja hakee
+    // Klikkaa hakukuvaketta
+    cy.get('.cdx-button.cdx-button--fake-button--enabled')
+      .first()
+      .click({ force: true });
+    // Etsii hakukentän ja hakee "Jamk"
     cy.get('#searchInput').type('Jamk{enter}', { force: true });
     // Tarkistaa, että olemme oikealla sivulla
     cy.url().should('include', 'Jyv%C3%A4skyl%C3%A4n_ammattikorkeakoulu');
     // Rullaa kohtaan "Kampukset" sivun sisällössä
     cy.get('#Kampukset').scrollIntoView();
-    // Tai vaihtoehtoisesti etsi h2-otsikko
-    cy.contains('h2', 'Kampukset').scrollIntoView();
+    // Tarkistaa että "Kampukset" otsikko on näkyvissä
+    cy.get('#Kampukset').should('be.visible');
+    // Odottaa 5 sekuntia
+    cy.wait(5000);
+    // Vaihtaa Kielen englanniksi, jonka jälkeen päädymme Jamkin englanninkieliselle sivulle
+    cy.get('#p-lang-btn').click();
+    cy.get('a[lang="en"]').click();
+    // Varmistaa että URL on muuttunut englanninkieliseksi
+    cy.url().should(
+      'contain',
+      'https://en.wikipedia.org/wiki/JAMK_University_of_Applied_Sciences'
+    );
   });
 });
+*/
