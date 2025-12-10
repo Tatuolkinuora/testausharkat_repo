b40cbe4edadbaa8f0600749641ee00e869a3778e
Tatuolkinuora <ag3462@student.jamk.fi>
2025-12-10 22:12:32 +0200

    tehtävä 2

--- end-to-end/cypress/e2e/spec.cy.js
+++ end-to-end/cypress/e2e/spec.cy.js
@@ -29,3 +29,31 @@ describe('My first Test', () => {
   });
 });
 */
+
+// Tehtävä 2
+// menee sivustolle https://tiko.jamk.fi/~imjar/fronttiper/esimteht/pizza_anim/
+describe('My first Test', () => {
+  it('Tehtävä 1', () => {
+    // Menee https://tiko.jamk.fi/~imjar/fronttiper/esimteht/pizza_anim/
+    cy.visit('https://tiko.jamk.fi/~imjar/fronttiper/esimteht/pizza_anim/');
+    // Täyttää Nimi-kentän tiedot, ja tarkistaa että ne ovat oikein
+    cy.get('#nimi').type('Matti Meikäläinen');
+    cy.get('#nimi').should('have.value', 'Matti Meikäläinen');
+    // Täyttää Puhelin-kentän tiedot, ja tarkistaa että ne ovat oikein
+    cy.get('#puhelin').type('0401234567');
+    cy.get('#puhelin').should('have.value', '0401234567');
+    //Täyttää Sähköposti-kentän tiedot, ja tarkistaa että ne ovat oikein
+    cy.get('#sposti').type('matti.meikalainen@example.com');
+    cy.get('#sposti').should('have.value', 'matti.meikalainen@example.com');
+    // Valitsee halutun koon (esim. Normaali)
+    cy.get('select#koko').select('Normaali');
+    // Valitsee halutun pohjan (esim. Ruis)
+    cy.get('#Ruis').check();
+    // Valitsee halutut täytteet (Salami, Kebab ja Ananas)
+    cy.get('#Salami').check();
+    cy.get('#Kebab').check();
+    cy.get('#Ananas').check();
+    // Tarkistaa, että “Maksa tilaus”-nappulan yläpuolella oleva hinta-elementissä on oikea loppusumma
+    cy.contains('Hinta: 12.00 e').should('be.visible');
+  });
+});
