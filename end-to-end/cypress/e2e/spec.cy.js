c2b2f1db750ed52828c8507ec966c4037b6fc53a
Tatuolkinuora <ag3462@student.jamk.fi>
2025-12-10 21:02:47 +0200

    blaa

--- end-to-end/cypress/e2e/spec.cy.js
+++ end-to-end/cypress/e2e/spec.cy.js
@@ -1,12 +1,14 @@
 describe('My first Test', () => {
   it('clicking "type" shows the right headings', () => {
-    cy.visit('https://example.cypress.io');
-    cy.wait(5000);
-    cy.contains('type').click();
-    cy.wait(5000);
-    cy.url().should('include', '/commands/actions');
-    cy.get('.action-email')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com');
+    // Menee suomenkiliselle Wikipedia etusivulle
+    cy.visit('https://fi.wikipedia.org/wiki/Wikipedia:Etusivu');
+    // Etsii hakukentän, kirjoittaa siihen "Jamk" ja hakee
+    cy.get('#searchInput').type('Jamk{enter}', { force: true });
+    // Tarkistaa, että olemme oikealla sivulla
+    cy.url().should('include', 'Jyv%C3%A4skyl%C3%A4n_ammattikorkeakoulu');
+    // Rullaa kohtaan "Kampukset" sivun sisällössä
+    cy.get('#Kampukset').scrollIntoView();
+    // Tai vaihtoehtoisesti etsi h2-otsikko
+    cy.contains('h2', 'Kampukset').scrollIntoView();
   });
 });
